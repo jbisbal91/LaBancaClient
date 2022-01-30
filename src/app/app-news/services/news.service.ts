0ef4b530257c506cb35648ccf6aab44f99e09139
jbisbal91 <jbisbal91@gmail.com>
2022-01-30 17:11:51 -0500

    service

--- src/app/app-news/services/news.service.ts
+++ src/app/app-news/services/news.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+import { News } from '../models/news';
+import { environment } from 'environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +16,21 @@ export class NewsService {
     protected http: HttpClient) {
   }
 
+  getAllNews(): Observable<News[]> {
+    return this.http.get<News[]>(`${environment.apiURL}/noticias`)
+        .pipe(
+            catchError(err => {
+                return throwError(err);
+            })
+        );
+      }
 
+  getNewsById(newsId: number): Observable<News> {
+    return this.http.get<News>(`${environment.apiURL}/noticias/${newsId}`)
+      .pipe(
+       catchError(err => {
+         return throwError(err);
+        })
+        );
+    }   
 }
