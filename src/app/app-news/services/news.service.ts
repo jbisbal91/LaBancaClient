81ec04c4ed088b23a6dbb05e887985ad12da49a4
jbisbal91 <jbisbal91@gmail.com>
2022-01-30 23:19:59 -0500

    fixed

--- src/app/app-news/services/news.service.ts
+++ src/app/app-news/services/news.service.ts
@@ -1,31 +1,28 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-import { News } from '../models/news';
-import { environment } from 'environments/environment';
-
-
+import { News } from "../models/news";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NewsService {
-
-  constructor(
-    protected http: HttpClient) {
-  }
+  constructor(protected http: HttpClient) {}
 
   getAllNews(): Observable<News[]> {
-    return this.http.get<News[]>('/noticias');
-      }
+    return this.http.get<News[]>("/noticias").pipe(
+      catchError((err) => {
+        return throwError(err);
+      })
+    );
+  }
 
   getNewsById(newsId: number): Observable<News> {
-    return this.http.get<News>(`/noticias/${newsId}`)
-      .pipe(
-       catchError(err => {
-         return throwError(err);
-        })
-        );
-    }   
+    return this.http.get<News>(`/noticias/${newsId}`).pipe(
+      catchError((err) => {
+        return throwError(err);
+      })
+    );
+  }
 }
