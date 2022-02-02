5ad33241cd50f1effd36c05489e15a2b7b7e3da1
jbisbal91 <jbisbal91@gmail.com>
2022-02-02 10:38:34 -0500

    fixed

--- src/app/app-news/components/news-details/news-details.component.ts
+++ src/app/app-news/components/news-details/news-details.component.ts
@@ -44,6 +44,17 @@ export class NewsDetailsComponent implements OnInit,OnDestroy {
     })
   }
 
+getUrl(value: string):string{
+  if(value!==undefined){
+    return value;
+  }else{
+    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRBhTyGxLk3vpmRRkC6qx1nLT4ho6ZeWIiOKDva7X4gxzhRGxHQT3hLNhRbJkJtnk13Bc&usqp=CAU';
+  }
+}
+
+
+
+
   getFormattedDate(date: string): string {
     return this.dateService.toShortDate(date);
   }
